4d9bc0ca5d2572b21e2fe0eba508c5c6561efb4c
Komiljon Baxromov <139528357+KomiljonDeveloper@users.noreply.github.com>
2023-11-08 07:52:31 +0500

    Time: 0 ms (100.00%), Space: 39.9 MB (44.13%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+      int y1, x1,x2,y2,x3,y3;
+        x1 = points[0][0];
+        y1 = points[0][1];
+        x2 = points[1][0];
+        y2 = points[1][1];
+        x3 = points[2][0];
+        y3 = points[2][1];
+
+        return (y1 - y2) * (x3 - x2) != (x1 - x2) * (y3 - y2);
+    }
+}
